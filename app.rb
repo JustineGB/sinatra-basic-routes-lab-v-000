0029466a39384030a25214229561e112c46c5b40
Justine B <justinegayton@gmail.com>
2018-10-19 19:17:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,9 +4,12 @@ class App < Sinatra::Base
   get '/name' do
     resp.status =  200
   end
+
   get '/hometown' do
-    "Hello, World!"
+    @hometown = Hometown.all
+    erb :'hometown/index.html.erb'
   end
+
   get '/favorite-song' do
     "Hello, World!"
   end
