6803a162867a63d1e6ed3ad284649dceb3e7f795
Justine B <justinegayton@gmail.com>
2018-10-19 19:23:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,13 +3,16 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/name' do
     "My name is #{name}"
+    status 200 
   end
 
   get '/hometown' do
   "My hometown is #{hometown}"
+  status 200 
   end
 
   get '/favorite-song' do
     "My favorite song is #{favorite-song}"
+    status 200 
   end
 end
